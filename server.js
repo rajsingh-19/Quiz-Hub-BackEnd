dd682746a0e6c4744d2c71403cf70b4ffc99d3d0
Raj Singh <rajsingh.19.vns@gmail.com>
2024-03-13 14:17:17 +0530

    little configuration

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const app = express(); 
+app.use(cors());
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.status(200).json("The Server is established");
+    res.send("The Server is established");
 });
 
 app.listen(process.env.PORT || PORT, () => {
